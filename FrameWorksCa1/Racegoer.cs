f4bf4d7c0a57ff9a58566f58c8d27ed0127ed2cc
jamzy75 <jamzymup@gmail.com>
2024-10-21 00:05:17 +0100

    Created a Racegoer class

---
+++ FrameWorksCa1/Racegoer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameWorksCA1
+{
+    internal class Racegoer : User
+    {
+        public string ticketNumber;
+        public string seatNumber;
+
+        public string TicketNumber
+        {
+            get { return ticketNumber; }
+            set { ticketNumber = value; }
+        }
+
+        public string SeatNumber
+        {
+            get { return seatNumber; }
+            set { seatNumber = value; }
+        }
+
+        // No-argument constructor
+        public Racegoer()
+        {
+        }
+
+        // Full constructor
+        public Racegoer(string ticketNumber, string seatNumber)
+        {
+            this.ticketNumber = ticketNumber;
+            this.seatNumber = seatNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"TicketNumber: {ticketNumber}, SeatNumber: {seatNumber}";
+        }
+    }
+}
