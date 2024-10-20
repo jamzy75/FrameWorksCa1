f67d3b14c51f602da3d1d71b790d65283300c726
jamzy75 <jamzymup@gmail.com>
2024-10-21 00:10:30 +0100

    Created Menu

--- FrameWorksCa1/Program.cs
+++ FrameWorksCa1/Program.cs
@@ -1,10 +1,81 @@
-﻿namespace FrameWorksCa1
+﻿using FrameWorksCA1;
+
+namespace FrameWorksCa1
 {
     internal class Program
     {
+        static RaceCourseManager raceCourseManager = new RaceCourseManager();
+        static List<HorseOwner> horseOwners = new List<HorseOwner>();
+        static List<Racegoer> racegoers = new List<Racegoer>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("\nHorse Racing Management System Menu:");
+                Console.WriteLine("1. Create a new Race Event (RaceCourse Manager)");
+                Console.WriteLine("2. Add a Race to an Event (RaceCourse Manager)");
+                Console.WriteLine("3. Upload a list of Horses to a Race (RaceCourse Manager)");
+                Console.WriteLine("4. Enter a Horse in a Race (Horse Owner)");
+                Console.WriteLine("5. View Upcoming Events and Races (Racegoer)");
+                Console.WriteLine("6. Exit");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        CreateRaceEvent();
+                        break;
+                    case "2":
+                        AddRaceToEvent();
+                        break;
+                    case "3":
+                        UploadHorsesToRace();
+                        break;
+                    case "4":
+                        EnterHorseInRace();
+                        break;
+                    case "5":
+                        ViewUpcomingEvents();
+                        break;
+                    case "6":
+                        running = false;
+                        Console.WriteLine("Exiting...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        private static void ViewUpcomingEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void EnterHorseInRace()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void UploadHorsesToRace()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AddRaceToEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CreateRaceEvent()
+        {
+            throw new NotImplementedException();
         }
     }
-}
+    }
+
