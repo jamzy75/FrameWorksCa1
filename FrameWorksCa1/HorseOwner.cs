830d87b3f3cb53433c3c9befaa90ff9860d970d1
jamzy75 <jamzymup@gmail.com>
2024-10-20 23:11:04 +0100

    Created HorseOwner class With method enterHorseInRace , that enters a horse in a race.

---
+++ FrameWorksCa1/HorseOwner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameWorksCA1
+{
+    internal class HorseOwner : User
+    {
+        public string ownerName;
+
+
+        public string OwnerName
+        {
+            get { return ownerName; }
+            set { ownerName = value; }
+
+        }
+
+        public HorseOwner()
+        {
+        }
+
+        // Full constructor
+        public HorseOwner(string ownerName)
+        {
+            this.ownerName = ownerName;
+        }
+
+        public void enterHorseInRace(Race race, Horse horse)
+        {
+            race.AddHorse(horse);
+        }
+
+
+        public override string ToString()
+        {
+            return $"OwnerName: {ownerName}";
+        }
+    }
+}
