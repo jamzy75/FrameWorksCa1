d51251b4a6b956379d261cc20f68557f99e83aa2
jamzy75 <jamzymup@gmail.com>
2024-10-20 23:35:55 +0100

    Created RaceCourseManager class Created CreateEvent method , that creates an event

---
+++ FrameWorksCa1/RaceCourseManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameWorksCA1
+{
+    internal class RaceCourseManager : User
+    {
+        private List<RaceEvent> Events = new List<RaceEvent>();
+
+        public void CreateEvent(string eventName, string location, int numberOfRaces)
+        {
+            RaceEvent raceEvent = new RaceEvent(eventName, location, numberOfRaces);
+            Events.Add(raceEvent);
+        }
+
+       
+       
+
+
+    }
+}
