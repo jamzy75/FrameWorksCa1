7a5a32a6b7313afd0a1b93c35dae8553198eec13
jamzy75 <jamzymup@gmail.com>
2024-10-20 22:28:39 +0100

    Horse class created

---
+++ FrameWorksCa1/Horse.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameWorksCA1
+{
+    internal class Horse
+    {
+        public string horseID;
+
+
+        public string horseName;
+
+        public DateTime dateOfBirth;
+
+
+
+        public string HorseID
+        {
+            get { return horseID; }
+            set { horseID = value; }
+
+        }
+
+        public string HorseName
+        {
+            get { return horseName; }
+            set { horseName = value; }
+
+        }
+
+        public DateTime DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set { dateOfBirth = value; }
+        }
+
+        // No-argument constructor
+        public Horse()
+        {
+        }
+
+        // Full constructor
+        public Horse(string horseID, string horseName, DateTime dateOfBirth)
+        {
+            this.horseID = horseID;
+            this.horseName = horseName;
+            this.dateOfBirth = dateOfBirth;
+        }
+
+
+        public override string ToString()
+        {
+            return $"HorseID: {horseID}, HorseName: {horseName}, DateOfBirth: {dateOfBirth}";
+        }
+
+    }
+
+
+}
